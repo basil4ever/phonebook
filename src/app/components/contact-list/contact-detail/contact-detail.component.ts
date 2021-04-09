49d13a4c094f42105d6da4529a5b3e81266169cb
basil4ever <basil4ever@mail.ru>
2021-04-09 11:19:15 +0300

    Phone book

--- src/app/components/contact-list/contact-detail/contact-detail.component.ts
+++ src/app/components/contact-list/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Contact} from '../../../models/contact.module';
 
 @Component({
   selector: 'app-contact-detail',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailComponent implements OnInit {
 
+  @Input() contact: Contact;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Contact} from '../../models/contact.module';
 import {ContactsService} from '../../services/contacts.service';
 
+
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
